d34ac093b614ad2a2d9aed6b6abb0bb921b93ce5
rqalbuquerque <rqa@cin.ufpe.br>
2017-11-10 01:41:10 -0200

    Initial Commit

---
+++ src/lbp.py
@@ -0,0 +1,8 @@
+# Lib with 
+#   - LBP functionalities
+#   - CLPB implementation
+# By: Renato Albuquerque (rqa@cin.ufpe.br)
+
+import numpy as np
+import cv2
+
---
+++ src/main.py
@@ -0,0 +1,13 @@
+import numpy as np
+import cv2
+
+img = cv2.imread('database/t1.jpg',0)
+cv2.imshow('image',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+def main():
+    # my code here
+    
+if __name__ == "__main__"
+    main()
